fc20f6fae96ab0c5dfd7d1d822c113dd91894e47
Peter Lieber <41489571+peteralieber@users.noreply.github.com>
2019-04-09 09:18:00 -0500

    addlot script for adding lots to a CSV file

---
+++ addlot.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Add Row to lot list file
+"""
+
+__author__ = "Peter Lieber"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import argparse
+import blapi
+import csv
+import pprint
+import os
+
+def main(args):
+    """ Main entry point of the app """
+    print("Add Lot")
+    print(args)
+    
+    pp = pprint.PrettyPrinter(indent=2).pprint
+    
+    if (False and args.init):
+      fields=['Id','Image','Part No', 'Name', 'Color', 'Quantity', 
+              'Average Price', 'Value']
+      with open(args.file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+    
+    id = 23
+    
+    if os.path.exists("lots.dat"):
+      with open ("lots.dat", "r") as datfile:
+        data=datfile.readlines()
+        if data:
+          id = int(data[0])
+    
+    color = args.color
+    image_color = 'White' if args.color.lower() == 'various' else args.color
+    bl = blapi.BL()
+    item = bl.getItemInfo('PART', args.part_no)
+    price = 0.1 if args.color.lower() == 'various' else bl.getAveragePrice('PART', args.part_no, args.color)
+    image = bl.getImage('PART', args.part_no, image_color)
+    quantity = int(args.qty if int(args.weight) <= 0 else (int(args.weight)-1)/float(item['weight']))
+    pp(item)
+    
+    fields=[id+1,
+            '=IMAGE("HTTP:{}")'.format(image['thumbnail_url']),
+            args.part_no,
+            item['name'],
+            color,
+            quantity,
+            price,
+            int(quantity)*float(price)
+            ]
+    pp(fields)
+    
+    #for i in fields: print(i)
+    #for i in fields: print(repr(i))
+    
+    with open(args.file, 'a', newline='') as f:
+      writer = csv.writer(f)
+      writer.writerow(fields)
+    
+    with open ("lots.dat", "w") as datfile:
+      datfile.write(str(id+1))
+    
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument("part_no", help="Lego Design ID")
+    parser.add_argument("qty", help="Quantity in Lot")
+    parser.add_argument("color", 
+      help="BL Color ID or Name")
+      
+    # Optional argument flag which defaults to False
+    parser.add_argument("-i", "--init", action="store_true", default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    #parser.add_argument("-n", "--name", action="store", dest="name")
+    
+    parser.add_argument("-w", "--weight", action="store", dest="weight", default=0,
+      help="Weight of item, in grams, to calculate quantity")
+    
+    parser.add_argument("-f", "--file", action="store", dest="file", default='lots.csv',
+      help="CSV File to store lots")
+    
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    #parser.add_argument(
+    #    "-v",
+    #    "--verbose",
+    #    action="count",
+    #    default=0,
+    #    help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)
--- blapi.py
+++ blapi.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+"""
+Module Docstring
+"""
 
-# using BL API
-# 
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
 
+import argparse
 import pprint
 import requests
 from requests_oauthlib import OAuth1
 import enum
+import csv
 
 ITEMTYPES = ["MINIFIG", "PART", "SET", "BOOK", "GEAR", "CATALOG", "INSTRUCTION", 
   "UNSORTED_LOT", "ORIGINAL_BOX"]
-
 
 
 class BL(object):
@@ -31,6 +36,16 @@ class BL(object):
       
   def getItemInfo(self, type, no):
     req = requests.get(self.base_url+'/items/{type}/{no}'.format(type=type, no=no), auth=self.oauth)
+    res = req.json()
+    if res['meta']['code'] != 200:
+      print('ERROR')
+      print(res['meta'])
+      return -1
+    else:
+      return res['data']
+      
+  def getImage(self, type, no, color='White'):
+    req = requests.get(self.base_url+'/items/{type}/{no}/images/{color}'.format(type=type, no=no, color=self.getColor(color)['color_id']), auth=self.oauth)
     res = req.json()
     if res['meta']['code'] != 200:
       print('ERROR')
@@ -66,11 +81,10 @@ class BL(object):
       
   def getAveragePrice(self, type, no, color):
     pg = self.getPriceGuide(type, no, color)
-    
     if not pg:
       return -1
     else:
-      return 1
+      return pg['avg_price']
   
   def getColors(self):
     return self.sendRequest('/colors')
@@ -88,31 +102,76 @@ class BL(object):
     else:
       return res['data']
 
-pp = pprint.PrettyPrinter(indent=2).pprint
+      
+def main(args):
+  """ Main entry point of the app """
+  print("BL API")
+  print(args)
+  test()
 
-bl = BL()
-pp(bl.getColor(4))
-print('')
-pp(bl.getColor('Dark Bluish Gray'))
-print('')
-pp(bl.getPriceGuide(type='PART', no='3623'))
 
-#pg = bl.getPriceGuide(type='SET', no='41597-1')
-#pp(pg)
+def test():
+  
+  pp = pprint.PrettyPrinter(indent=2).pprint
 
-#subset = bl.getSubsets(type='SET', no='41597-1')
-#pp(subset)
+  bl = BL()
+  pp(bl.getColor(4))
+  print('')
+  pp(bl.getColor('Dark Bluish Gray'))
+  print('')
+  pp(bl.getPriceGuide(type='PART', no='3623', color='Blue'))
+  print('')
+  print(bl.getAveragePrice(type='PART', no='3623', color='Blue'))
 
-#orders = bl.getOrders()
-#pp(orders)
+  #pg = bl.getPriceGuide(type='SET', no='41597-1')
+  #pp(pg)
 
-#pp(bl.getColors())
+  #subset = bl.getSubsets(type='SET', no='41597-1')
+  #pp(subset)
 
-#req = requests.get(base_url+'/colors/4', auth=oauth)
-#res = req.json()
-#if res['meta']['code'] != 200:
-#  print('ERROR')
-#  pp(res['meta'])
-#else:
-#  pp(res['data'])
+  #orders = bl.getOrders()
+  #pp(orders)
+
+  #pp(bl.getColors())
+
+  #req = requests.get(base_url+'/colors/4', auth=oauth)
+  #res = req.json()
+  #if res['meta']['code'] != 200:
+  #  print('ERROR')
+  #  pp(res['meta'])
+  #else:
+  #  pp(res['data'])
+
+  
+if __name__ == "__main__":
+  """ This is executed when run from the command line """
+  parser = argparse.ArgumentParser()
+
+  # Required positional argument
+  #parser.add_argument("arg", help="Required positional argument")
+
+  # Optional argument flag which defaults to False
+  #parser.add_argument("-f", "--flag", action="store_true", default=False)
+
+  # Optional argument which requires a parameter (eg. -d test)
+  #parser.add_argument("-n", "--name", action="store", dest="name")
+
+  # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+  parser.add_argument(
+    "-v",
+    "--verbose",
+    action="count",
+    default=0,
+    help="Verbosity (-v, -vv, etc)")
+
+  # Specify output of "--version"
+  parser.add_argument(
+    "--version",
+    action="version",
+    version="%(prog)s (version {version})".format(version=__version__))
+
+  args = parser.parse_args()
+  main(args)
+    
+
 
